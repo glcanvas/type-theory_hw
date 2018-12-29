8638032f0b52dd12c3e94c8250f560dd8a08d596
glcanvas <duginetsn98@gmail.com>
2018-12-29 19:17:14 +0300

    solve without derivation of sub expression

--- c.ml
+++ c.ml
@@ -22,11 +22,18 @@ let rec  write_to_buffer buffer expression=
 *)
 
 module AlgebraMap = Map.Make(String);;
+
 module AbstractVar = Map.Make(String);;
+
 exception MatchEquation of string;;
 
 type algebra =  Var of string (* constant *)
             |   Func of string * algebra * algebra (* for expression like -> smth smth *);;
+
+type tmp_derivation =   OneArg of algebra
+                        | TwoArg of algebra * tmp_derivation
+                        | ThreeArg of algebra * tmp_derivation * tmp_derivation ;;
+
 let common_variable = "a";;
 
 let custom_merge a b = List.rev_append a b;;
@@ -48,30 +55,35 @@ let update_variable var =
                                 let new_var = new_var ^ Char.escaped (Char.chr ((Char.code symb) + 1) ) in
                                     new_var;;
 
- let rec build_system expression free_var abstaction_name =
+ let rec build_system expression free_var abstaction_name unbound_names =
     let token = expression in
         match token with
-            | Application (a, b) ->  let union_a, term_a, last_map_a, last_var_a =  build_system a free_var abstaction_name in
-                                        let  union_b, term_b, last_map_b, last_var_b = build_system b last_var_a abstaction_name in
+            | Application (a, b) ->  let union_a, term_a, last_map_a, last_unbound_map_a, last_var_a =  build_system a free_var abstaction_name unbound_names in
+                                        let  union_b, term_b, last_map_b, last_unbound_map_b, last_var_b = build_system b last_var_a abstaction_name last_unbound_map_a in
                                             let new_var = update_variable last_var_b in
                                                 let merged_equation = custom_merge union_a union_b in
                                                     let merged_map = AbstractVar.fold AbstractVar.add last_map_a last_map_b in
                                                             let added_equation = merged_equation @ [(term_a, (Func ("->", term_b, Var(new_var))))] in
-                                                        (added_equation, Var(new_var), merged_map, new_var)
+                                                        (added_equation, Var(new_var), merged_map, last_unbound_map_b, new_var)
 
             | Abstraction (a, b) ->     let new_free_var = update_variable free_var in
                                             let updated_map = AbstractVar.add a new_free_var abstaction_name in
-                                                    let union_b, term_b, last_map_b, last_var_b = build_system b new_free_var updated_map in
+                                                    let union_b, term_b, last_map_b, last_unbound_map_b, last_var_b = build_system b new_free_var updated_map unbound_names in
                                                         let new_var = update_variable last_var_b in
-                                                            (union_b, Func ("->", Var(new_free_var), term_b), last_map_b, new_var)
+                                                            (union_b, Func ("->", Var(new_free_var), term_b), last_map_b, last_unbound_map_b, new_var)
 
             | Variable (a)       ->     let contains_variable = AbstractVar.mem a abstaction_name in
                                             if contains_variable then
                                                 let variable_name = AbstractVar.find a abstaction_name in
-                                                    (  [], Var(variable_name), abstaction_name, free_var)
+                                                    (  [], Var(variable_name), abstaction_name, unbound_names, free_var)
                                             else
-                                                let new_var = update_variable free_var in
-                                                    (  [], Var(new_var), abstaction_name, new_var) ;;
+                                                let unbound_var = AbstractVar.mem a unbound_names in
+                                                    match unbound_var with
+                                                    | true -> let var_name = AbstractVar.find a unbound_names in
+                                                                ([], Var(var_name), abstaction_name, unbound_names, free_var)
+                                                    | false -> let new_var = update_variable free_var in
+                                                                    let added_bound_names = AbstractVar.add a new_var unbound_names in
+                                                                        (  [], Var(new_var), abstaction_name, added_bound_names, new_var) ;;
 
 let encure_array eq_system =
     let double_list = List.map (fun x-> (x,(fst x, snd x))) eq_system in
@@ -114,6 +126,41 @@ let rec replace_var_in_expression expression variable template =
                                Func(a,left_replace,right_replace)
                                ;;
 
+let rec check_exist_recursion variable equations =
+    match equations with
+    | Var(a) -> Var(a) = variable
+    | Func(a,b,c) -> let left = check_exist_recursion variable b in
+                        let right = check_exist_recursion variable c in
+                            left || right;;
+
+let rec check_same_derivation equations =
+    match equations with
+    | [] -> true
+    | _ -> let left,right = List.hd equations in
+                let tail = List.tl equations in
+                    match left with
+                    | Var(a) -> let result = check_exist_recursion (Var a) right in
+                                    if  result then false else check_same_derivation tail
+                    | _ -> check_same_derivation tail;;
+
+let rec help_delete_repeat left right equations =
+    match equations with
+    | [] -> []
+    | _ -> let head_left, head_right =  List.hd equations in
+                let tail = List.tl equations in
+                    if head_left = left && head_right = right then
+                        help_delete_repeat left right tail
+                    else
+                        [(head_left, head_right)] @ (help_delete_repeat left right tail);;
+
+let rec delete_repeat equations =
+    match equations with
+    | [] -> []
+    | _ -> let head_left, head_right =  List.hd equations in
+                let tail = List.tl equations in
+                    let result_delete = help_delete_repeat head_left head_right tail in
+                        [(head_left, head_right)] @ (delete_repeat result_delete);;
+
 let fourth_action_replace variable template equations =
     List.map (fun item ->
                     let left, right = item in
@@ -137,7 +184,7 @@ let fourth_action equations =
                                                                                 | None, None -> None
                                                                                 | None, k2 -> k2
                                                                                 | k1, None -> k1
-                                                                            ) a b) (AbstractVar.empty) tmp_map_list in
+                                                                              ) a b) (AbstractVar.empty) tmp_map_list in
                                                   (*AbstractVar.iter (fun k v -> Printf.printf "%s <=> %d\n" k v) result_map; for debug *)
                     let filtered_map = AbstractVar.filter (fun k v -> v > 1) result_map in
                         let list_of_vars_term = available_map equations in
@@ -152,10 +199,7 @@ let fourth_action equations =
                                                 | 0 -> equations
                                                 | _ -> let var, template = List.hd filtered_list in
                                                             fourth_action_replace var template equations;;
-(*
-define triple values x y z
-from conspect
-*)
+
 let rec help_calculate_num_of_vars expr mp =
     match expr with
     | Var(a) -> if AbstractVar.mem a mp then
@@ -167,45 +211,7 @@ let rec help_calculate_num_of_vars expr mp =
                             let result_c = help_calculate_num_of_vars c result_b in
                                 result_c;;
 
-(* first number *)
-let calculate_first item =
-    let first = fst item in
-    let emp_mp = AbstractVar.empty in
-        let result = help_calculate_num_of_vars first emp_mp in
-            let ok_vars = AbstractVar.filter (fun a b -> b > 1) result in
-                AbstractVar.cardinal ok_vars;;
-(*end first number*)
 
-(*second number*)
-let rec help_calc_second exp =
-        match exp with
-            | Var(a) -> 0
-            | Func(a, b, c) -> (help_calc_second b) + (help_calc_second c) + 1;;
-
-let calculate_second item =
-        let f = help_calc_second (fst item) in
-            let s = help_calc_second (snd item) in
-               f + s;;
-(*end second number*)
-
-(*third number*)
-let calculate_third item =
-    match item with
-    | Var(a), Var(b) -> if a = b then  1 else 0
-    | Func(b, c, d), Var(a) -> 1
-    | _ -> 0;;
-(*end third number*)
-(*
-let calculate_statement equations =
-                    let array_of_tuple = List.map
-                        (fun it-> (calculate_first it, calculate_second it, calculate_third it))
-                            equations in
-                    let result_tuple = List.fold_left (fun a b -> let n1, n2, n3 = b in
-                                                                    let a1, a2, a3 = a in
-                                                                         (n1 + a1, n2 + a2, n3 + a3))
-                                                                            (0,0,0) array_of_tuple in
-       result_tuple;;
-*)
 let rec is_solved_form equations var_map =
     match equations with
     | [] -> true
@@ -222,8 +228,6 @@ let rec is_solved_form equations var_map =
                         | Func(a, b, c) -> false;;
 
 let rec solve_equations equations =
-    List.iter (fun it -> Printf.printf "%s | %s \n" (algebra_to_string (fst it)) (algebra_to_string (snd it))) equations;
-    print_string "!!!!\n";
     (*term = var -> var = term*)
     let first = List.map (fun item -> if (not (is_var (fst item))) && (is_var (snd item)) then ((snd item), (fst item)) else item) equations in
         (*term = term -> [equiv_1..equiv_n]*)
@@ -236,58 +240,53 @@ let rec solve_equations equations =
                         | _ -> true
                 ) second in
                     (* replace all occur variable more than one time*)
-                    let fourth = fourth_action delete_equal in
-                        let compare = is_solved_form fourth (AbstractVar.empty) in
-                            match compare with
-                                | false -> solve_equations fourth
-                                | true -> fourth;;
+                    let exist_recursion = check_same_derivation delete_equal in
+                        if not exist_recursion then
+                            (false, delete_equal)
+                        else
+                            let deleted_repeat = delete_repeat delete_equal in
+                            let fourth = fourth_action deleted_repeat in
+                                let compare = is_solved_form fourth (AbstractVar.empty) in
+                                    match compare with
+                                        | false -> (*List.iter (fun a -> print_string ((algebra_to_string (fst a)) ^ " = ");
+                                                                                  print_string ((algebra_to_string (snd a)  ) ^ "\n")) fourth;
+                                                                                  print_string "=============\n"; print for debug*)
+                                                    solve_equations fourth
+                                        | true -> (true, fourth);;
 
 (*
 \f.\x.(f(f x))
 \f.(\x. f (x x)) (\x. f (x x))
 (\x.x)(\y.y)(\x.x)(\x.x)(\x.x)(\x.x)(\x.x)(\x.x)(\x.x)(\x.x)(\x.x)(\x.x)
+\x.\y.\z.(x z)(y z)
 *)
 
-let e = Lexing.from_string "\f.\x. f (f x)";;
+let e = Lexing.from_string "\f.\x.f(f(f(f(f(f(f(f x)))))))";;
 let d =(Parser.lambdaParser Lexer.main)  e;;
 
 
-
-
 let empty_bounded_names = AbstractVar.empty;;
-let my_equitatins, lambda_type, bounded_names, last_free_name = build_system d common_variable empty_bounded_names;;
+let emplty_unbounded_names = AbstractVar.empty;;
+let my_equitatins, lambda_type, bounded_names, unbounded_names, last_free_name = build_system d common_variable empty_bounded_names emplty_unbounded_names;;
 
+print_string "equations: \n";;
 List.iter (fun a -> print_string ((algebra_to_string (fst a)) ^ " = ");
                                print_string ((algebra_to_string (snd a)  ) ^ "\n")) my_equitatins;;
-print_string "=======\n";;
+
+print_string "bounded names = \n";;
 AbstractVar.iter (fun a b ->  print_string ("key: " ^ a ^ " value: " ^ b ^ "\n")) bounded_names;;
-print_string "==========\n";;
+
+print_string "!!!! unbounded names = \n";;
+AbstractVar.iter (fun a b ->  print_string ("key: " ^ a ^ " value: " ^ b ^ "\n")) unbounded_names;;
+
+print_string "expression type = \n";;
 print_string ((algebra_to_string lambda_type) ^ "\n");;
 
+let ok,eq =  solve_equations my_equitatins;; (*[(Var "c",Var "e");(Var "d", Var "e");(Var "d", Var "e");(Var "e", Var "f"); ]*)
 
-let eq =  encure_array [
-(Var("d"), Var("c"));
-(Var("e"), Var("d"));
-](*my_equitatins*) (*[
-
-(Var("a"),Var("b"));
-(Var("c"), Func("->", Var("a"), Var("e")));
-(Func("->", Var("a"), Var("b")), Var("a"));
-(Func("->", Var("a"), Var("b")), Func("->", Var("a"), Var("b")));
-(Var("a"), Var("a"));
-(Func("->", Var("b"), Func("->", Var("a"), Var("a"))), Var("a"));
-(Var("f"),Var("b"));
-(Var("c"), Func("->", Var("a"), Var("e")));
-]*);;
-
-let tmp_eq = fourth_action eq;;
-let tmp_eq =fourth_action tmp_eq;;
-let tmp_eq =fourth_action tmp_eq;;
-let tmp_eq =fourth_action tmp_eq;;
-print_string "=====|||==\n";;
-List.iter (fun a -> print_string ((algebra_to_string (fst a)) ^ " = ");
-                                         print_string ((algebra_to_string (snd a)  ) ^ "\n")) tmp_eq;;
-(*
-let n1,n2,n3 = calculate_statement eq;;
-Printf.printf "%d %d %d" n1 n2 n3;;
-*)
+if not ok then
+    Printf.printf "Expression has no type\n"
+else
+    print_string "solved system=\n";
+    List.iter (fun a -> print_string ((algebra_to_string (fst a)) ^ " = ");
+                                                 print_string ((algebra_to_string (snd a)  ) ^ "\n")) eq;;
